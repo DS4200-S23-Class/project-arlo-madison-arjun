79907879e0edcc0e5774750fac8f227909624860
arjunkarkare <91080637+arjunkarkare@users.noreply.github.com>
2023-03-20 13:48:29 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,39 +1,12 @@
-// // Import the SheetJS library
-// import * as JS from 'xlsx'
 
-// // Load the Excel file
-// const data_file = JS.readFile('data.xlsx');
+const launch_data = d3.csv("Data/launch.csv").then((data) => {
+  console.log(data); 
+});
 
-// // Get the sheet names
-// const sheets = data_file.SheetNames;
+const recorded_data = d3.csv("Data/recorded.csv").then((data) => {
+  console.log(data); 
+});
 
-// // Loop through each sheet and get the data
-// sheetNames.forEach(name => {
-//   const worksheet = data_file.Sheets[name];
-//   const data = XLSX.utils.sheet_to_json(worksheet);
-  
-//   // Do something with the data, such as rendering a D3 visualization
-// console.log(data);});
-
-
-// First, we need a frame  
-const FRAME_HEIGHT = 200;
-const FRAME_WIDTH = 500; 
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
-
-const FRAME1 = d3.select("#vis1") 
-                  .append("svg") 
-                    .attr("height", FRAME_HEIGHT)   
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame"); 
-
-// Next, open file 
-d3.csv("data2.csv").then((data) => { 
-
-  // d3.csv parses a csv file 
-  // .then() passes the data parsed from the file to a function
-  // in the body of this function is where you will build your 
-  // vis 
-
-  // let's check our data
-  console.log(data); });
+const proposed_data = d3.csv("Data/proposed.csv").then((data) => {
+  console.log(data); 
+});
